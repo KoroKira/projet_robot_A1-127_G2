633bcdb9a46fc287980f1722d5abbf9046a3783d
Guilhem Desarcy-Lemiere <63927791+KoroKira@users.noreply.github.com>
2023-04-19 13:40:13 +0200

    Update bouton_fonction.ino
    je continuerai le code spécifiquement de cette fonction a postériori

--- bouton_fonction.ino
+++ bouton_fonction.ino
@@ -1,1 +1,26 @@
-ffff
+//code du groupe 2 pour le projet ecotech - installation de boutons
+//Mettez des commentaires bande de sacripans, c'est important pour que les gens qui repassent sur le code comprennent les changements faits et puissent être efficace après <3
+//Contributeurs: Guilhem Desarcy-Lemiere, Thomas Del Gatto, Luca Desplat, Kiara Vachet, Charlyne Bary (cablage essentiellement mais c'est déjà ça), Luca Fraissine et Méline Torrent
+
+//Définition valeur de bouton marche
+int button_marche = 0;
+
+//Définition valeur de bouton ARU
+int button_ARU = 0;
+
+// Déclaration de la variable avant la fonction loop()
+
+// Déclarations de fonctions
+
+void setup()
+{
+  //Définition des broches comme entrées/sorties
+  pinMode(button_marche, OUTPUT);
+  pinMode(button_ARU, OUTPUT);
+  
+}
+
+void loop()
+{
+  
+}
